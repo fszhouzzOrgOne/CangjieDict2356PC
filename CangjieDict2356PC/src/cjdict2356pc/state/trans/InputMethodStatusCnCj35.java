ac1de6d92fa8480656a05275c04bde11d52f279c
fszhouzz <fszhouzz@qq.com>
2020-05-02 21:56:29 +0800

    三代依Arthurmcarthur建議，z鍵中文字母改爲片字，今五代也改爲片

--- CangjieDict2356PC/src/cjdict2356pc/state/trans/InputMethodStatusCnCj35.java
+++ CangjieDict2356PC/src/cjdict2356pc/state/trans/InputMethodStatusCnCj35.java
@@ -1,6 +1,7 @@
 package cjdict2356pc.state.trans;
 
 import java.util.List;
+import java.util.Map;
 
 import cjdict2356pc.dto.Item;
 import cjdict2356pc.mb.MbUtils;
@@ -10,6 +11,14 @@ public class InputMethodStatusCnCj35 extends InputMethodStatusCnCj {
     public InputMethodStatusCnCj35() {
         this.setSubType(MbUtils.TYPE_CODE_CJGEN35);
         this.setSubTypeName("倉35");
+    }
+    
+    @Override
+    public Map<String, Object> getKeysNameMap() {
+        Map<String, Object> mbTransMap = super.getKeysNameMap();
+        // 三代依Arthurmcarthur建議，z鍵中文字母改爲片字，今五代也改爲片。
+        mbTransMap.put("z", "片");
+        return mbTransMap;
     }
 
     @Override
--- CangjieDict2356PC/src/cjdict2356pc/state/trans/InputMethodStatusCnCj5.java
+++ CangjieDict2356PC/src/cjdict2356pc/state/trans/InputMethodStatusCnCj5.java
@@ -1,6 +1,7 @@
 package cjdict2356pc.state.trans;
 
 import java.util.List;
+import java.util.Map;
 
 import cjdict2356pc.dto.Item;
 import cjdict2356pc.mb.MbUtils;
@@ -10,6 +11,14 @@ public class InputMethodStatusCnCj5 extends InputMethodStatusCnCj {
     public InputMethodStatusCnCj5() {
         this.setSubType(MbUtils.TYPE_CODE_CJGEN5);
         this.setSubTypeName("倉5");
+    }
+    
+    @Override
+    public Map<String, Object> getKeysNameMap() {
+        Map<String, Object> mbTransMap = super.getKeysNameMap();
+        // 三代依Arthurmcarthur建議，z鍵中文字母改爲片字，今五代也改爲片。
+        mbTransMap.put("z", "片");
+        return mbTransMap;
     }
 
     @Override
